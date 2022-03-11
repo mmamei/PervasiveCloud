b3bcbd6d6ef6365eda22c43c27d6a9599c52424b
Marco Mamei <marco.mamei@unimore.it>
2022-03-11 10:40:44 +0100

    added python exercises w libraries

---
+++ Lezione 04 - Python-venv-git/web.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def main():
+    return 'ciao'
+
+@app.route('/home')
+def home():
+    return redirect(url_for('static', filename='home.html'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
