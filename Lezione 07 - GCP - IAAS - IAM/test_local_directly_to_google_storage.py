d6be0038448ea85edc87e513bca9b1e8b86439d4
Marco Mamei <marco.mamei@unimore.it>
2022-03-25 11:26:36 +0100

    added some text files for Lesson 7

---
+++ Lezione 07 - GCP - IAAS - IAM/test_local_directly_to_google_storage.py
@@ -0,0 +1,10 @@
+from google.cloud import storage
+client = storage.Client.from_service_account_json('credentials.json')
+#bucket = client.create_bucket('upload-mamei-1')
+bucket = client.bucket('upload-mamei-1')
+source_file_name = 'test.jpg'
+destination_blob_name = source_file_name
+blob = bucket.blob(destination_blob_name)
+blob.upload_from_filename(source_file_name)
+print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+
