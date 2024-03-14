3aa4f5356119dbf3fa2990d9163483be7f52cf82
mmamei <marco.mamei@unimore.it>
2024-03-14 16:12:20 +0100

    added some examples

---
+++ Lezione 07 - GCP - IAAS - IAM/finance.py
@@ -0,0 +1,7 @@
+import yfinance as yf
+import datetime
+start_ts = datetime.date.today() - datetime.timedelta(days = 2)
+ticker = 'PHAU.L'
+data = yf.download(ticker, start=start_ts).reset_index()
+
+
