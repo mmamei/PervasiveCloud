0a39891bc9809ba7d48954ce92728424b851b1ad
mmamei <marco.mamei@unimore.it>
2025-03-14 13:58:36 +0100

    fix chatbpt

--- Lezione 05 - FrontEnd/2_Chatbot/bot.py
+++ Lezione 05 - FrontEnd/2_Chatbot/bot.py
@@ -1,59 +1,37 @@
-# pip install python-telegram-bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
-from secret import bot_token
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from secret import key
 
-def welcome(update, context):
-    msg = '''Welcome in <b>My Bot</b>'''
-    update.message.reply_text(msg, parse_mode='HTML')
+async def hello(update, context):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-def process_chat(update, context):
-    print(context)
-    if update.message.text.lower() == 'avvisi':
-        msg = 'Non ci sono nuovi avvisi'
-        update.message.reply_text(msg, parse_mode='HTML')
-    elif update.message.text.lower() == 'graph':
-        chat_id = update.message.chat.id
-        context.bot.send_document(chat_id=chat_id, document=open('photo.jpg', 'rb'))
-    else:
-        welcome(update, context)
+async def echo(update, context):
+    await update.message.reply_text(update.message.text)
 
-def process_location(update, context):
+async def photo_handler(update, context):
+    user = update.message.from_user
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive("chatbot/img/user_photo.jpg")
+    #await update.message.reply_text(f'photo received {user}')
+    chat_id = update.message.chat.id
+    await context.bot.send_document(chat_id=chat_id, document=open('chatbot/img/user_photo.jpg', 'rb'))
 
+async def process_location(update, context):
     if update.edited_message:
         message = update.edited_message
     else:
         message = update.message
-
     user_location = message.location
 
     user = message.from_user
-    print(user)
     print(f"You talk with user {user['first_name']} and his user ID: {user['id']}")
-
     msg = f'Ti trovi presso lat={user_location.latitude}&lon={user_location.longitude}'
-    print(msg)
-    message.reply_text(msg)
+    await message.reply_text(msg)
 
-def photo_handler(update, context):
-    file = context.bot.getFile(update.message.photo[-1].file_id)
-    file.download('photo.jpg')
-    update.message.reply_text('photo received')
-
-def main():
-   print('bot started')
-   upd= Updater(bot_token, use_context=True)
-   disp=upd.dispatcher
-
-   disp.add_handler(CommandHandler("start", welcome))
-   disp.add_handler(MessageHandler(Filters.regex('^.*$'), process_chat))
-   disp.add_handler(MessageHandler(Filters.location, process_location))
-   disp.add_handler(MessageHandler(Filters.photo, photo_handler))
-
-
-   upd.start_polling()
-   upd.idle()
-
-
-
-if __name__=='__main__':
-   main()
+print('starting')
+app = ApplicationBuilder().token(key).build()
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+app.add_handler(MessageHandler(filters.PHOTO, photo_handler))
+app.add_handler(MessageHandler(filters.LOCATION, process_location))
+app.run_polling()
